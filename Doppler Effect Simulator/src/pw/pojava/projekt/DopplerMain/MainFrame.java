bf821c7fd224b4755c6bf6fba2eacf7b7226c6f9
Maciej <Maciej@Maciejowa-Chata>
2018-03-14 18:35:02 +0100

    First commit. Added: -MainFrame class; -in MainFrame: set basic frame configuration.

---
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/MainFrame.java
@@ -0,0 +1,20 @@
+package pw.pojava.projekt.DopplerMain;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public MainFrame(){
+		this.setSize(860,640);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		JFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+	}
+
+}
