7d5df28970d701a815a3fe1c3ad6738e115e444b
Jakub <Jakub@10.68.17.85>
2018-03-15 11:02:54 +0100

    Added class GUI

---
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/GUI.java
@@ -0,0 +1,28 @@
+package pw.pojava.projekt.DopplerMain;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class GUI extends JPanel {
+
+	public GUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUI(LayoutManager arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUI(boolean arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUI(LayoutManager arg0, boolean arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
