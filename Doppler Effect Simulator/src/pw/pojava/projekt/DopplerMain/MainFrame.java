27b34c802c925b5b1daed77744454c9a8289a3bd
Maciej <Maciej@Maciejowa-Chata>
2018-03-20 17:51:25 +0100

    Added frame title.

--- Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/MainFrame.java
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/MainFrame.java
@@ -10,6 +10,7 @@ public class MainFrame extends JFrame {
 	public MainFrame(){		
 		this.setSize(860,640);
 		this.setResizable(false);//zeby rozmiar okna byl staly
+		this.setTitle("Symulator efektu Dopplera");
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
