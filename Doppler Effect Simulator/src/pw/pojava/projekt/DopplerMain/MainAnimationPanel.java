363377e20c7c52e8c6a387c209c71a2745e74e88
Maciej <Maciej@Maciejowa-Chata>
2018-04-05 11:48:54 +0200

    Implemented methods adding AnimationObjects and Waves in MainAnimationPanel.

--- Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/MainAnimationPanel.java
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/MainAnimationPanel.java
@@ -13,7 +13,7 @@ import javax.swing.JPanel;
 public class MainAnimationPanel extends JPanel implements Runnable {
 	
 	List<WaveCrest> crests = new ArrayList<WaveCrest>();
-	
+	AnimationObject observer1, observer2, source;
 	
 
 	Dimension preferredSize = new Dimension(530,400);
@@ -44,7 +44,36 @@ public class MainAnimationPanel extends JPanel implements Runnable {
 				break;
 		}
 	}
+	public void addObserver1(int vx, int vy, double x, double y) {
+		observer1 = new AnimationObject();
+		observer1.setX(x);
+		observer1.setY(y);
+		observer1.setVx(vx);
+		observer1.setVy(vy);		
+	}
+	public void addObserver2(int vx, int vy, double x, double y, Color color) {
+		observer2 = new AnimationObject();
+		observer2.setX(x);
+		observer2.setY(y);
+		observer2.setVx(vx);
+		observer2.setVy(vy);
+	}
+	public void addSource(int vx, int vy, double x, double y, Color color) {
+		source = new AnimationObject();
+		source.setX(x);
+		source.setY(y);
+		source.setVx(vx);
+		source.setVy(vy);
+	}
+	public void addWaveCrest(int v, double x, double y, double r) {
+		WaveCrest c = new WaveCrest();
+		c.setX(x);
+		c.setY(y);
+		c.setV(v);
+		c.setR(r);
 		
+		crests.add(c);
+	}
 }
 
 	
