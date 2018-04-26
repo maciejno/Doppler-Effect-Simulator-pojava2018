3e946efa525b39d68feaaef9d5b7983cd7ed1439
Jakub <Jakub@10.68.30.136>
2018-04-26 11:56:39 +0200

    Still trying

--- Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/WaveCrest.java
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/WaveCrest.java
@@ -14,17 +14,17 @@ public class WaveCrest { //grzbiet fali
 		super();
 	}
 
-	public void paint(Graphics g){
+	public synchronized void paint(Graphics g){
         g.setColor(color);
         //rzutuje double na int
         g.drawOval((int)(x-r), (int)(y-r), (int)(2*r), (int)(2*r));//szerokosc okregu 2r, przesuwa polozenie jego lewego gornego rogu tak by srodek okregu byl w punkcie x,y(czyli jak szerokosc 2r, to o r)
     }
 	
-	public void setV(int v) {this.v = v;}
+	public synchronized void setV(int v) {this.v = v;}
 	public int getV() {return v;}
-	public void setX(double x) {this.x = x;}
-	public void setY(double y) {this.y = y;}
-	public void setR(double r) {this.r = r;}
+	public synchronized void setX(double x) {this.x = x;}
+	public synchronized void setY(double y) {this.y = y;}
+	public synchronized void setR(double r) {this.r = r;}
 	public double getX() {return x;}
 	public double getY() {return y;}
 	public double getR() {return r;}
