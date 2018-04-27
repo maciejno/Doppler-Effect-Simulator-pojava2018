702a1e6213a479a713e581bc4eb2345826ec8acc
Maciej <Maciej@Maciejowa-Chata>
2018-04-27 19:23:01 +0200

    Improved source chart. Set Y axis range for all charts.

--- Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/SourceAnimationPanel.java
+++ Doppler Effect Simulator/src/pw/pojava/projekt/DopplerMain/SourceAnimationPanel.java
@@ -1,27 +1,19 @@
 package pw.pojava.projekt.DopplerMain;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
 
 import javax.swing.SwingWorker;
 
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 public class SourceAnimationPanel extends ChartPanel{
 
-	private static final long serialVersionUID = 1L;
-	
-	XYSeriesCollection xySeriesCollection;//pola
-	XYSeries xySeries;					//przepisane
-	JFreeChart chart;					//przez konstruktor
-	MySwingWorker worker;
-	Double x,y;
-	
+	private static final long serialVersionUID = 1L;											
+	XYSeries xySeries;									
+	SourceSwingWorker worker;	
+	Double x,y;	
 	GUI superior;//referencja do GUI
 	
 	public SourceAnimationPanel(GUI superior) {
@@ -29,10 +21,10 @@ public class SourceAnimationPanel extends ChartPanel{
 		xySeries = new XYSeries("Source signal");
 		superior.sourceCollection.addSeries(xySeries);
 		this.superior = superior;
-		worker = new MySwingWorker();
+		worker = new SourceSwingWorker();
 	}
 
-	class MySwingWorker extends SwingWorker<Void,XYDataItem>{
+	class SourceSwingWorker extends SwingWorker<Void,XYDataItem>{
 		
 		double pi = Math.PI;
 		double time =0;//aktualna chwila czasu
@@ -40,18 +32,23 @@ public class SourceAnimationPanel extends ChartPanel{
 		int maxCount = 3000;//maksymalna liczba punktow na wykresie * freq
 		
 		
-		MySwingWorker(){}
+		SourceSwingWorker(){
+			xySeries.clear(); //usuwa wszystkie dane z serii
+			for(int i=0; i<maxCount/((double)superior.soundFreq/100);i++) {
+				xySeries.add(time-i,0.0);//wype³nia zerami dane
+			}
+		}
 		//UWAGA: jest dzielenie freq przez 100, zeby pracowalo dla szerszego zakresu czestotliwosci - dzieki temu jest do 10-15kHzkHz, a nie do 100-150Hz
 		@Override
- 	   protected void process(List<XYDataItem> dane) {
+ 	   	protected void process(List<XYDataItem> dane) {//dodaje dane do serii i jak jest ich za duzo to usuwa
  		   	for(XYDataItem d : dane) {
  		   		xySeries.add(d);
- 		   	if(xySeries.getItemCount()>maxCount/((double)superior.soundFreq/100))//if(xySeries.getItemCount()>500)//jak sie zmieni wartosc maxCount, to szerokosc inna
+ 		   	while(xySeries.getItemCount()>maxCount/((double)superior.soundFreq/100))//if(xySeries.getItemCount()>500)//jak sie zmieni wartosc maxCount, to szerokosc inna
  		   			xySeries.remove(0);	//to na gorze co zakomentowane jesli ma sie nie dostosowywac do czestotliwosci szerokosc okna 
  		   	}
- 	   }
+ 	   	}
 		@Override
-		protected Void doInBackground() throws Exception {
+		protected Void doInBackground() throws Exception {//oblicza wartosi sinusa, czas w ms i przesyla do process 
 				while(superior.isRunning) {
 					x = new Double(time);
 					y = new Double(Math.sin(2*pi*((double)superior.soundFreq/100)*time/1000));
@@ -62,12 +59,10 @@ public class SourceAnimationPanel extends ChartPanel{
 				}
 			return null;
 		}
-		
 	}
-
 	
-	public void newWorker() {
-		worker = new MySwingWorker();		
+	public void newWorker() {//metoda do tworzenia nowego swing workera
+		worker = new SourceSwingWorker();		
 	}
 
 }
